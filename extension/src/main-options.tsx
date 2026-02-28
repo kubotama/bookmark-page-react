6f276ee68410fe7e1d43c040fba921c98376ef7e
kubotama <16697573+kubotama@users.noreply.github.com>
2026-02-28 21:41:29 +0900

    refactor(extension): エントリポイントの React 型参照をリファクタリング
    - main-popup.tsx, main-options.tsx: React.StrictMode を StrictMode に変更
    - 未使用の import React を削除

--- extension/src/main-options.tsx
+++ extension/src/main-options.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Options } from './Options'
 import './style.css'
@@ -6,8 +6,8 @@ import './style.css'
 const root = document.getElementById('root')
 if (root) {
   createRoot(root).render(
-    <React.StrictMode>
+    <StrictMode>
       <Options />
-    </React.StrictMode>
+    </StrictMode>,
   )
 }
--- extension/src/main-popup.tsx
+++ extension/src/main-popup.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Popup } from './Popup'
 import './style.css'
@@ -6,8 +6,8 @@ import './style.css'
 const root = document.getElementById('root')
 if (root) {
   createRoot(root).render(
-    <React.StrictMode>
+    <StrictMode>
       <Popup />
-    </React.StrictMode>,
+    </StrictMode>,
   )
 }
